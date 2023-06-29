0d557b50cdb973ac5000d3d8da997224d044b580
khoi le <lmkhoi2k@gmail.com>
2023-06-29 16:36:10 +0700

    remove marketing

--- src/layouts/main/nav/config-navigation.ts
+++ src/layouts/main/nav/config-navigation.ts
@@ -4,21 +4,21 @@ import { paths } from 'src/routes/paths';
 // ----------------------------------------------------------------------
 
 export const pageLinks = [
-  {
-    order: '1',
-    subheader: 'Marketing',
-    cover: '/assets/images/menu/menu_marketing.jpg',
-    items: [
-      { title: 'Landing', path: paths.marketing.landing },
-      { title: 'Services', path: paths.marketing.services },
-      { title: 'Case Studies', path: paths.marketing.caseStudies },
-      { title: 'Case Study', path: paths.marketing.caseStudy },
-      { title: 'Blog Posts', path: paths.marketing.posts },
-      { title: 'Blog Post', path: paths.marketing.post },
-      { title: 'About', path: paths.marketing.about },
-      { title: 'Contact', path: paths.marketing.contact },
-    ],
-  },
+  // {
+  //   order: '1',
+  //   subheader: 'Marketing',
+  //   cover: '/assets/images/menu/menu_marketing.jpg',
+  //   items: [
+  //     { title: 'Landing', path: paths.marketing.landing },
+  //     { title: 'Services', path: paths.marketing.services },
+  //     { title: 'Case Studies', path: paths.marketing.caseStudies },
+  //     { title: 'Case Study', path: paths.marketing.caseStudy },
+  //     { title: 'Blog Posts', path: paths.marketing.posts },
+  //     { title: 'Blog Post', path: paths.marketing.post },
+  //     { title: 'About', path: paths.marketing.about },
+  //     { title: 'Contact', path: paths.marketing.contact },
+  //   ],
+  // },
   // {
   //   order: '6',
   //   subheader: 'Travel',
